e3d520a371c4c2ada123789f8fa0c1f17cef1e07
Courtney Letsa <courtneyletsa@gmail.com>
2024-03-09 17:18:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <button className='App-button'>
+          Enter Gallery →
+        </button>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
