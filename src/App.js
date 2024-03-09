711e9c577f0cc0aa6549083771999356be71f104
Courtney Letsa <courtneyletsa@gmail.com>
2024-03-09 19:30:39 +0000

    route added successfully

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+// import logo from './logo.svg';
+import Splash from './pages/Splash';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className='App-button'>
-          Enter Gallery →
-        </button>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Splash />} />
+    </Routes>
   );
 }
 
